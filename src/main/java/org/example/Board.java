a54f943760b3aad77ecadd7c08b08a5590b614ff
Samin <samin.agha@yahoo.com>
2024-01-08 18:32:48 +0100

    Print func implemented

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -15,7 +15,16 @@ public class Board {
     public boolean isCellEmpty(int x, int y) {
         return cells[x][y] == '\u0000'; // Check if cell is empty
     }
-    public void print(){
-
+    public void print() {
+        System.out.println("▁▁▁▁▁▁");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                char cellValue = (cells[i][j] == '\u0000') ? ' ' : cells[i][j];
+                System.out.print(cellValue + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("▔▔▔▔");
     }
-    }
+}
