34ce2b454a94e00df5c4c5337826331514372836
Eteri <ic22b031@technikum-wien.ac.at>
2024-01-08 21:43:31 +0100

    call game.start()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kill the world!");
+        TicTacToe game = new TicTacToe();
+        game.start();
     }
 }
