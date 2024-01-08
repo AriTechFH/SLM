c7396957bba47b552ee9da023d6fa9ae17cec898
Samin <samin.agha@yahoo.com>
2024-01-08 18:25:33 +0100

    Creat board and place symbol functionality

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class Board {
-}
+
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+    }
+
+    public void place(int x, int y, char marker) {
+        cells[x][y] = marker;
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == '\u0000'; // Check if cell is empty
+    }
+    public void print(){
+
+    }
+    }
---
+++ src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,40 @@
+package org.example;
+import java.util.Scanner;
+
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    public TicTacToe() {
+        player1 = new Player('O');
+        player2 = new Player('X');
+        currentPlayer = player1;
+        board = new Board();
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        boolean gameWon = false;
+
+        while (!gameWon) {
+            board.print();
+            System.out.println("Current Player: " + currentPlayer.getMarker());
+
+            System.out.print("Row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.print("Column (0-2): ");
+            int column = scanner.nextInt();
+
+            if (board.isCellEmpty(row, column)) {
+                board.place(row, column, currentPlayer.getMarker());
+            } else {
+                System.out.println("Cell already occupied. Try again.");
+            }
+        }
+
+        board.print();
+    }
+}
+
