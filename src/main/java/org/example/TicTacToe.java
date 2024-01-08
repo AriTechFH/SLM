118bc8cbcb98549dea78ae14d12baccb035a8f47
Eteri <ic22b031@technikum-wien.ac.at>
2024-01-08 19:18:57 +0100

    option to restart implented

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -17,24 +17,32 @@ public class TicTacToe {
     public void start() {
         Scanner scanner = new Scanner(System.in);
         boolean gameWon = false;
+        String playAgain = "Y";
 
-        while (!gameWon) {
-            board.print();
-            System.out.println("Current Player: " + currentPlayer.getMarker());
+        do{
+            while (!gameWon) {
+                board.print();
+                System.out.println("Current Player: " + currentPlayer.getMarker());
 
-            System.out.print("Row (0-2): ");
-            int row = scanner.nextInt();
-            System.out.print("Column (0-2): ");
-            int column = scanner.nextInt();
+                System.out.print("Row (0-2): ");
+                int row = scanner.nextInt();
+                System.out.print("Column (0-2): ");
+                int column = scanner.nextInt();
 
-            if (board.isCellEmpty(row, column)) {
-                board.place(row, column, currentPlayer.getMarker());
-            } else {
-                System.out.println("Cell already occupied. Try again.");
+                if (board.isCellEmpty(row, column)) {
+                    board.place(row, column, currentPlayer.getMarker());
+                } else {
+                    System.out.println("Cell already occupied. Try again.");
+                }
             }
-        }
+            board.print();
 
-        board.print();
+            System.out.println("Möchten Sie nocheinmal spielen? Y/N");
+            playAgain = scanner.next();
+
+        }while(playAgain.equals("Y"));
+
+
     }
 }
 
